a6cc297afeb106dfbb944d151523e152e9f20044
Owen Davies <owendavies1993@hotmail.com>
2012-03-15 16:48:46 +0000

    Implemented Main, need to analyse the algorithms now.

--- src/dictionary/Main.java
+++ src/dictionary/Main.java
@@ -1,6 +1,11 @@
 package dictionary;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Random;
+
+import dictionary.InsertComplexities.InstrumentedKey;
 
 public class Main {
 
@@ -8,14 +13,35 @@ public class Main {
     private static final int REPITITIONS = 500;
 
     public static void main(String[] args) throws FileNotFoundException {
-        BinarySearchTree<Integer, String> l =
-                new BinarySearchTree<Integer, String>();
 
-        l.put(4, "hello");
-        l.put(7, "hi");
-        l.put(1, "sup");
-        l.put(2, "fuck");
-        l.put(5, "shit");
-        l.remove(2);
+        Random rand = new Random();
+        InsertComplexities iC = new InsertComplexities(rand);
+        PrintStream pS;
+
+        File oFile =
+                new File(System.getProperty("user.dir")
+                        + "\\OrderedLinkedList.dat");
+
+        int[] oComplexities =
+                iC.getInsertComplexities(
+                        new OrderedLinkedList<InstrumentedKey, Integer>(),
+                        MAX_SIZE, REPITITIONS);
+        pS = new PrintStream(oFile);
+        for (int i = 0; i < REPITITIONS; i++) {
+            pS.println(i + "\t" + oComplexities[i]);
+        }
+
+        File bFile =
+                new File(System.getProperty("user.dir")
+                        + "\\BinarySearchTree.dat");
+
+        int[] bComplexities =
+                iC.getInsertComplexities(
+                        new OrderedLinkedList<InstrumentedKey, Integer>(),
+                        MAX_SIZE, REPITITIONS);
+        pS = new PrintStream(bFile);
+        for (int i = 0; i < REPITITIONS; i++) {
+            pS.println(i + "\t" + bComplexities[i]);
+        }
     }
 }
